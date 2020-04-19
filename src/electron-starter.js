525ddab71f416495ebe25c1fad8fc59da99e8c28
Radoslav Stankov <rstankov@gmail.com>
2020-04-19 18:34:12 +0300

    Add electron menu bar

---
+++ src/electron-starter.js
@@ -0,0 +1,12 @@
+const { menubar } = require('menubar');
+
+const mb = menubar({
+  index: 'http://localhost:3000',
+});
+
+mb.on('ready', () => {
+  console.log('app is ready');
+  // your app code here
+});
+
+mb.app.allowRendererProcessReuse = false;
