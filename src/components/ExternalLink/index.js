e78b3a97eecb43f8705137cba144cc48d45381cf
Radoslav Stankov <rstankov@gmail.com>
2020-04-22 22:16:36 +0300

    Fix lint issue

--- src/components/ExternalLink/index.js
+++ src/components/ExternalLink/index.js
@@ -1,5 +1,9 @@
 import * as React from 'react';
 
-export default function ExternalLink(props) {
-  return <a target="_blank" rel="noopener noreferrer" {...props} />;
+export default function ExternalLink({ children, ...props }) {
+  return (
+    <a target="_blank" rel="noopener noreferrer" {...props}>
+      {children}
+    </a>
+  );
 }
