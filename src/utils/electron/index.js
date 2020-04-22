a8428e6ae4d269c89fe33a0c8a3b312c6ff25bad
Radoslav Stankov <rstankov@gmail.com>
2020-04-22 23:15:56 +0300

    Add utilities for writing and reading task files

--- src/utils/electron/index.js
+++ src/utils/electron/index.js
@@ -38,3 +38,68 @@ export function resizeBasedOnContent(height) {
   });
   observer.observe(document.body);
 }
+
+const fs = window.require && window.require('fs').promises;
+
+const FILE_VERSION = 1;
+
+export async function writeStoreToFile(store) {
+  if (!isElectron) {
+    return;
+  }
+
+  const { filePath } = await electron.remote.dialog.showSaveDialog({
+    defaultPath:
+      '~/Desktop/' +
+      store.task.title.toLocaleLowerCase().replace(/\W+/g, '_') +
+      '.json',
+  });
+
+  if (!filePath) {
+    return;
+  }
+
+  const content = JSON.stringify({
+    version: FILE_VERSION,
+    date: new Date(),
+    store,
+  });
+
+  try {
+    await fs.writeFile(filePath, content);
+  } catch (e) {
+    alert(`An error occurred while writing the file: ${e.message}`);
+  }
+}
+
+export async function readStoreFromFile() {
+  if (!isElectron) {
+    return;
+  }
+
+  const { filePaths } = await electron.remote.dialog.showOpenDialog({
+    properties: ['openFile'],
+    filters: [{ name: 'JSON', extensions: ['json'] }],
+  });
+
+  if (!filePaths || filePaths.length === 0) {
+    return;
+  }
+
+  const [filePath] = filePaths;
+
+  try {
+    const data = await fs.readFile(filePath, 'utf-8');
+
+    const restore = JSON.parse(data);
+
+    if (restore.version !== FILE_VERSION) {
+      alert(`Unsupported file version: ${restore.version}`);
+      return;
+    }
+
+    return restore.store;
+  } catch (e) {
+    alert(`An error occurred while reading the file: ${e.message}`);
+  }
+}
