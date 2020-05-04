8d2e0edfaed684aa75e570fd9569c662886e8320
Radoslav Stankov <rstankov@gmail.com>
2020-05-04 21:53:48 +0300

    Focus on end of input

--- app/src/utils/focusOn.tsx
+++ app/src/utils/focusOn.tsx
@@ -1,4 +1,10 @@
 export default function focusOn(id: string) {
-  const el = document.getElementById(id);
-  el && el.focus();
+  const el = document.getElementById(id) as any;
+
+  if (!el) {
+    return;
+  }
+
+  el.focus();
+  el.setSelectionRange(el.value.length, el.value.length);
 }
