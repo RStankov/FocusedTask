34c4efcabe13572244c0b4b652b1d841a0ec7b82
Radoslav Stankov <rstankov@gmail.com>
2020-05-10 22:56:48 +0300

    Use drag & drop to sort bookmarks and todos

--- app/src/components/TaskBookmarks/index.tsx
+++ app/src/components/TaskBookmarks/index.tsx
@@ -9,6 +9,7 @@ import focusOn from 'utils/focusOn';
 import useSelector from 'hooks/useSelector';
 import useDispatch from 'hooks/useDispatch';
 import { openURI } from 'utils/electron';
+import Sortable from 'components/Sortable';
 
 import {
   updateBookmark,
@@ -26,70 +27,85 @@ export default function TaskBookmarks() {
 
   return (
     <Stack.Column gap="xs">
-      {bookmarks.map((bookmark, i) => (
-        <Stack.Row
-          key={bookmark.id}
-          className={styles.row}
-          onClick={e => {
-            if (e.metaKey && isUrl(bookmark)) {
-              e.preventDefault();
-              openURI(bookmark.uri);
-            }
-          }}>
-          {isUrl(bookmark) ? (
-            <ExternalLink href={bookmark.uri} className={styles.number}>
-              {i < 9 ? i + 1 : i === bookmarks.length - 1 ? 0 : ''}
-            </ExternalLink>
-          ) : (
-            <div className={styles.inactive} />
-          )}
-          <InputText
-            type="text"
-            id={`bookmark-${i}`}
-            autoFocus={!!bookmark.autoFocus}
-            value={bookmark.uri}
-            placeholder="https://example.com"
-            onChange={e =>
-              dispatch(updateBookmark({ id: bookmark.id, uri: e.target.value }))
-            }
-            onPaste={e => {
-              const clipboard = e.clipboardData.getData('Text');
-
-              if (clipboard.indexOf('\n') === -1) {
-                return;
+      <Sortable.List
+        onSort={({ oldIndex, newIndex }: any) =>
+          dispatch(
+            moveBookmark({
+              id: bookmarks[oldIndex]!.id,
+              by: newIndex - oldIndex,
+            }),
+          )
+        }>
+        {bookmarks.map((bookmark, i) => (
+          <Sortable.Item
+            index={i}
+            key={bookmark.id}
+            as={Stack.Row}
+            className={styles.row}
+            onClick={(e: any) => {
+              if (e.metaKey && isUrl(bookmark)) {
+                e.preventDefault();
+                openURI(bookmark.uri);
               }
-
-              e.preventDefault();
-
-              dispatch(pasteBookmarks({ id: bookmark.id, clipboard }));
-            }}
-            onKeyDown={e => {
-              if (e.target.value === '' && e.keyCode === keyCodes.backspace) {
-                dispatch(removeBookmark(bookmark));
-                focusOnBookmarkWithIndex(i - 1);
-              } else if (e.keyCode === keyCodes.enter) {
-                dispatch(newBookmark({ after: bookmark }));
-              } else if (e.keyCode === keyCodes.esc) {
-                e.target.blur();
-              } else if (!e.metaKey && e.keyCode === keyCodes.up) {
-                focusOnBookmarkWithIndex(i - 1);
-              } else if (!e.metaKey && e.keyCode === keyCodes.down) {
-                focusOnBookmarkWithIndex(i + 1);
-              } else if (e.metaKey && e.keyCode === keyCodes.up) {
-                dispatch(moveBookmark({ id: bookmark.id, by: -1 }));
-              } else if (e.metaKey && e.keyCode === keyCodes.down) {
-                dispatch(moveBookmark({ id: bookmark.id, by: +1 }));
+            }}>
+            <Sortable.Handle />
+            {isUrl(bookmark) ? (
+              <ExternalLink href={bookmark.uri} className={styles.number}>
+                {i < 9 ? i + 1 : i === bookmarks.length - 1 ? 0 : ''}
+              </ExternalLink>
+            ) : (
+              <div className={styles.inactive} />
+            )}
+            <InputText
+              type="text"
+              id={`bookmark-${i}`}
+              autoFocus={!!bookmark.autoFocus}
+              value={bookmark.uri}
+              placeholder="https://example.com"
+              onChange={e =>
+                dispatch(
+                  updateBookmark({ id: bookmark.id, uri: e.target.value }),
+                )
               }
-            }}
-            onBlur={() => dispatch(removeBookmarkAutoFocus(bookmark))}
-          />
-          {isUrl(bookmark) && (
-            <ExternalLink href={bookmark.uri} className={styles.link}>
-              <Emoji emoji="↗️" />
-            </ExternalLink>
-          )}
-        </Stack.Row>
-      ))}
+              onPaste={e => {
+                const clipboard = e.clipboardData.getData('Text');
+
+                if (clipboard.indexOf('\n') === -1) {
+                  return;
+                }
+
+                e.preventDefault();
+
+                dispatch(pasteBookmarks({ id: bookmark.id, clipboard }));
+              }}
+              onKeyDown={e => {
+                if (e.target.value === '' && e.keyCode === keyCodes.backspace) {
+                  dispatch(removeBookmark(bookmark));
+                  focusOnBookmarkWithIndex(i - 1);
+                } else if (e.keyCode === keyCodes.enter) {
+                  dispatch(newBookmark({ after: bookmark }));
+                } else if (e.keyCode === keyCodes.esc) {
+                  e.target.blur();
+                } else if (!e.metaKey && e.keyCode === keyCodes.up) {
+                  focusOnBookmarkWithIndex(i - 1);
+                } else if (!e.metaKey && e.keyCode === keyCodes.down) {
+                  focusOnBookmarkWithIndex(i + 1);
+                } else if (e.metaKey && e.keyCode === keyCodes.up) {
+                  dispatch(moveBookmark({ id: bookmark.id, by: -1 }));
+                } else if (e.metaKey && e.keyCode === keyCodes.down) {
+                  dispatch(moveBookmark({ id: bookmark.id, by: +1 }));
+                }
+              }}
+              onBlur={() => dispatch(removeBookmarkAutoFocus(bookmark))}
+            />
+            {isUrl(bookmark) && (
+              <ExternalLink href={bookmark.uri} className={styles.link}>
+                <Emoji emoji="↗️" />
+              </ExternalLink>
+            )}
+          </Sortable.Item>
+        ))}
+      </Sortable.List>
     </Stack.Column>
   );
 }
--- app/src/components/TaskTodos/index.tsx
+++ app/src/components/TaskTodos/index.tsx
@@ -7,6 +7,7 @@ import keyCodes from 'utils/keyCodes';
 import focusOn from 'utils/focusOn';
 import useSelector from 'hooks/useSelector';
 import useDispatch from 'hooks/useDispatch';
+import Sortable from 'components/Sortable';
 
 import {
   newTodo,
@@ -25,88 +26,107 @@ export default function TaskTodos() {
 
   return (
     <Stack.Column>
-      {todos.map((todo, i) => (
-        <div key={todo.id} style={{ paddingLeft: 20 * (todo.ident || 0) }}>
-          <Stack.Row align="start">
-            <div className={styles.box}>
-              {todo.isCompleted ? (
-                <Emoji
-                  emoji={'✅'}
-                  onClick={e => {
-                    e.stopPropagation();
-                    dispatch(toggleTodo(todo));
-                  }}
-                />
-              ) : (
-                <div
-                  className={styles.checkbox}
-                  onClick={e => {
-                    e.stopPropagation();
-                    dispatch(toggleTodo(todo));
-                  }}
-                />
-              )}
-            </div>
-            <InputTextarea
-              className={todo.isCompleted ? styles.completed : undefined}
-              id={`todo-text-${i}`}
-              autoFocus={!!todo.autoFocus}
-              multiline={false}
-              value={todo.text}
-              placeholder="..."
-              onChange={e =>
-                dispatch(
-                  updateTodoText({
-                    id: todo.id,
-                    text: e.target.value,
-                  }),
-                )
-              }
-              onPaste={e => {
-                const clipboard = e.clipboardData.getData('Text');
-
-                if (clipboard.indexOf('\n') === -1) {
-                  return;
+      <Sortable.List
+        onSort={({ oldIndex, newIndex }: any) =>
+          dispatch(
+            moveTodo({
+              id: todos[oldIndex]!.id,
+              by: newIndex - oldIndex,
+            }),
+          )
+        }>
+        {todos
+          .filter(todo => !!todo)
+          .map((todo, i) => (
+            <Sortable.Item
+              index={i}
+              align="start"
+              as={Stack.Row}
+              key={todo.id}
+              style={{ paddingLeft: 20 * (todo.ident || 0) }}>
+              <Sortable.Handle />
+              <div className={styles.box}>
+                {todo.isCompleted ? (
+                  <Emoji
+                    emoji={'✅'}
+                    onClick={e => {
+                      e.stopPropagation();
+                      dispatch(toggleTodo(todo));
+                    }}
+                  />
+                ) : (
+                  <div
+                    className={styles.checkbox}
+                    onClick={e => {
+                      e.stopPropagation();
+                      dispatch(toggleTodo(todo));
+                    }}
+                  />
+                )}
+              </div>
+              <InputTextarea
+                className={todo.isCompleted ? styles.completed : undefined}
+                id={`todo-text-${i}`}
+                autoFocus={!!todo.autoFocus}
+                multiline={false}
+                value={todo.text}
+                placeholder="..."
+                onChange={e =>
+                  dispatch(
+                    updateTodoText({
+                      id: todo.id,
+                      text: e.target.value,
+                    }),
+                  )
                 }
+                onPaste={e => {
+                  const clipboard = e.clipboardData.getData('Text');
 
-                e.preventDefault();
+                  if (clipboard.indexOf('\n') === -1) {
+                    return;
+                  }
 
-                dispatch(pasteTasks({ id: todo.id, clipboard }));
-              }}
-              onKeyDown={e => {
-                if (e.target.value === '' && e.keyCode === keyCodes.backspace) {
-                  dispatch(removeTodo(todo));
-                  focusOnTodoWithIndex(i - 1);
-                } else if (e.keyCode === keyCodes.enter) {
                   e.preventDefault();
-                  dispatch(newTodo({ after: todo }));
-                } else if (e.keyCode === keyCodes.esc) {
-                  e.target.blur();
-                } else if (!e.metaKey && e.keyCode === keyCodes.up) {
-                  focusOnTodoWithIndex(i - 1);
-                } else if (!e.metaKey && e.keyCode === keyCodes.down) {
-                  focusOnTodoWithIndex(i + 1);
-                } else if (e.metaKey && e.keyCode === keyCodes.up) {
-                  dispatch(moveTodo({ id: todo.id, by: -1 }));
-                } else if (e.metaKey && e.keyCode === keyCodes.down) {
-                  dispatch(moveTodo({ id: todo.id, by: +1 }));
-                } else if (e.metaKey && e.keyCode === keyCodes['[']) {
-                  dispatch(updateTodoIdent({ id: todo.id, by: -1 }));
-                } else if (e.metaKey && e.keyCode === keyCodes[']']) {
-                  dispatch(updateTodoIdent({ id: todo.id, by: 1 }));
-                } else if (
-                  e.metaKey &&
-                  e.shiftKey &&
-                  e.keyCode === keyCodes.c
-                ) {
-                  dispatch(toggleTodo(todo));
-                }
-              }}
-              onBlur={() => dispatch(removeTodoAutoFocus(todo))}
-            />
-          </Stack.Row>
-        </div>
-      ))}
+
+                  dispatch(pasteTasks({ id: todo.id, clipboard }));
+                }}
+                onKeyDown={e => {
+                  if (
+                    e.target.value === '' &&
+                    e.keyCode === keyCodes.backspace
+                  ) {
+                    dispatch(removeTodo(todo));
+                    focusOnTodoWithIndex(i - 1);
+                  } else if (e.keyCode === keyCodes.enter) {
+                    e.preventDefault();
+                    dispatch(newTodo({ after: todo }));
+                  } else if (e.keyCode === keyCodes.esc) {
+                    e.target.blur();
+                  } else if (!e.metaKey && e.keyCode === keyCodes.up) {
+                    focusOnTodoWithIndex(i - 1);
+                  } else if (!e.metaKey && e.keyCode === keyCodes.down) {
+                    focusOnTodoWithIndex(i + 1);
+                  } else if (e.metaKey && e.keyCode === keyCodes.up) {
+                    dispatch(moveTodo({ id: todo.id, by: -1 }));
+                  } else if (e.metaKey && e.keyCode === keyCodes.down) {
+                    dispatch(moveTodo({ id: todo.id, by: +1 }));
+                  } else if (e.metaKey && e.keyCode === keyCodes['[']) {
+                    dispatch(updateTodoIdent({ id: todo.id, by: -1 }));
+                  } else if (e.metaKey && e.keyCode === keyCodes[']']) {
+                    dispatch(updateTodoIdent({ id: todo.id, by: 1 }));
+                  } else if (
+                    e.metaKey &&
+                    e.shiftKey &&
+                    e.keyCode === keyCodes.c
+                  ) {
+                    dispatch(toggleTodo(todo));
+                  }
+                }}
+                onBlur={() => dispatch(removeTodoAutoFocus(todo))}
+              />
+            </Sortable.Item>
+          ))}
+      </Sortable.List>
       <div className={styles.actions}>
         <button onClick={() => dispatch(newTodo())}>new task</button>
       </div>
