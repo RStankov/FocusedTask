63f3130d350a0ebdabeab7ea16099a473553d922
Radoslav Stankov <rstankov@gmail.com>
2020-07-25 19:00:38 +0300

    Improve creating of new tasks

--- app/src/modules/tasks/index.ts
+++ app/src/modules/tasks/index.ts
@@ -1,4 +1,5 @@
 import taskReducer, { ITask } from '../task';
+import generateId from 'utils/generateId';
 
 type IState = {
   tasks: { [key: string]: ITask };
@@ -15,6 +16,143 @@ interface IAction {
   payload?: any;
 }
 
+export default function tasksReducer(
+  state: IState | undefined,
+  action: IAction,
+): IState {
+  if (state === undefined) {
+    state = ACTION_HANDLERS['tasks/new'](state);
+  }
+
+  const handler = ACTION_HANDLERS[action.type] || forwardAction;
+  return handler(state, action);
+}
+
+const UNDO_EMPTY = {
+  lastAction: null,
+  past: [],
+  future: [],
+};
+
+function createNewTask() {
+  return {
+    id: generateId('task'),
+    title: 'Focus',
+    todos: [],
+    bookmarks: [],
+    note: '',
+  };
+}
+
+const ACTION_HANDLERS: any = {
+  'tasks/new': (state: IState | undefined) => {
+    const newTask = createNewTask();
+
+    return {
+      selected: newTask.id,
+      tasks: {
+        ...(state?.tasks || {}),
+        [newTask.id]: newTask,
+      },
+      undo: UNDO_EMPTY,
+    };
+  },
+
+  'tasks/select': (state: IState, action: IAction) => {
+    const taskId = action.payload.task.id;
+
+    if (!state.tasks[taskId]) {
+      return state;
+    }
+
+    return {
+      selected: taskId,
+      tasks: state.tasks,
+      undo: UNDO_EMPTY,
+    };
+  },
+
+  'tasks/delete': (state: IState, action: IAction) => {
+    const taskId = action.payload.task.id;
+
+    if (!state.tasks[taskId]) {
+      return state;
+    }
+
+    const tasks = { ...state.tasks };
+
+    Reflect.deleteProperty(tasks, taskId);
+
+    let selected = state.selected;
+
+    if (selected === taskId) {
+      selected = Object.keys(tasks)[0];
+
+      if (!selected) {
+        const newTask = createNewTask();
+        selected = newTask.id;
+        tasks[selected] = newTask;
+      }
+    }
+
+    return {
+      selected,
+      tasks,
+      undo: UNDO_EMPTY,
+    };
+  },
+
+  undo: (state: IState, action: IAction) => {
+    const { past, future } = state.undo;
+
+    if (past.length === 0) {
+      return state;
+    }
+
+    const previous = past[past.length - 1];
+    const newPast = past.slice(0, past.length - 1);
+
+    return {
+      selected: state.selected,
+      tasks: {
+        ...state.tasks,
+        [state.selected]: previous,
+      },
+
+      undo: {
+        lastAction: action.type,
+        past: newPast,
+        future: [state.tasks[state.selected], ...future],
+      },
+    };
+  },
+
+  redo: (state: IState, action: IAction) => {
+    const { past, future } = state.undo;
+
+    if (future.length === 0) {
+      return state;
+    }
+
+    const next = future[0];
+    const newFuture = future.slice(1);
+
+    return {
+      selected: state.selected,
+      tasks: {
+        ...state.tasks,
+        [state.selected]: next,
+      },
+
+      undo: {
+        lastAction: action.type,
+        past: [...past, state.tasks[state.selected]],
+        future: newFuture,
+      },
+    };
+  },
+};
+
 const MAX_UNDO = 100;
 
 const UNDO_SKIP_ACTIONS = {
@@ -22,162 +160,48 @@ const UNDO_SKIP_ACTIONS = {
   'task/updateBookmark': 'task/newBookmark',
 } as any;
 
-const task = taskReducer(undefined as any, {} as any);
+function forwardAction(state: IState, action: IAction) {
+  const presentTask = state.tasks[state.selected];
 
-const emptyUndo = {
-  lastAction: null,
-  past: [],
-  future: [],
-};
-
-const initialState: IState = {
-  tasks: { [task.id]: task },
-  selected: task.id,
-  undo: emptyUndo,
-};
-
-export default function tasksReducer(
-  state = initialState,
-  action: IAction,
-): IState {
-  const { past, future } = state.undo || {};
-
-  switch (action.type) {
-    case 'tasks/new':
-      const newTask = taskReducer(undefined as any, { type: 'reset' });
-
-      return {
-        selected: newTask.id,
-        tasks: {
-          ...state.tasks,
-          [newTask.id]: newTask,
-        },
-        undo: emptyUndo,
-      };
-
-    case 'tasks/select':
-      if (!state.tasks[action.payload.task.id]) {
-        return state;
-      }
-
-      return {
-        selected: action.payload.task.id,
-        tasks: state.tasks,
-        undo: emptyUndo,
-      };
-
-    case 'tasks/delete':
-      if (!state.tasks[action.payload.task.id]) {
-        return state;
-      }
-
-      const tasks = { ...state.tasks };
-
-      Reflect.deleteProperty(tasks, action.payload.task.id);
-
-      let selected = state.selected;
-
-      if (selected === action.payload.task.id) {
-        selected = Object.keys(tasks)[0];
-
-        if (!selected) {
-          const replaceTask = taskReducer(undefined as any, { type: 'reset' });
-          selected = replaceTask.id;
-          tasks[selected] = replaceTask;
-        }
-      }
-
-      return {
-        selected,
-        tasks,
-        undo: emptyUndo,
-      };
-
-    case 'tasks/undo':
-      if (past.length === 0) {
-        return state;
-      }
-
-      const previous = past[past.length - 1];
-      const newPast = past.slice(0, past.length - 1);
-
-      return {
-        selected: state.selected,
-        tasks: {
-          ...state.tasks,
-          [state.selected]: previous,
-        },
-
-        undo: {
-          lastAction: action.type,
-          past: newPast,
-          future: [state.tasks[state.selected], ...future],
-        },
-      };
-
-    case 'tasks/redo':
-      if (future.length === 0) {
-        return state;
-      }
-
-      const next = future[0];
-      const newFuture = future.slice(1);
-
-      return {
-        selected: state.selected,
-        tasks: {
-          ...state.tasks,
-          [state.selected]: next,
-        },
-
-        undo: {
-          lastAction: action.type,
-          past: [...past, state.tasks[state.selected]],
-          future: newFuture,
-        },
-      };
-
-    default:
-      const task = state.tasks[state.selected];
-
-      if (!task) {
-        return state;
-      }
-
-      const updatedTask = taskReducer(task, action);
-
-      if (updatedTask === task) {
-        return state;
-      }
-
-      let undo = state.undo;
-      if (
-        UNDO_SKIP_ACTIONS[action.type] &&
-        UNDO_SKIP_ACTIONS[action.type] === state.undo.lastAction
-      ) {
-        undo = {
-          lastAction: action.type,
-          past,
-          future,
-        };
-      } else {
-        undo = {
-          lastAction: action.type,
-          past: [
-            ...(past.length > MAX_UNDO ? past.slice(1, MAX_UNDO) : past),
-            task,
-          ],
-          future: [],
-        };
-      }
-
-      return {
-        selected: updatedTask.id,
-        tasks: {
-          ...state.tasks,
-          [updatedTask.id]: updatedTask,
-        },
-        undo,
-      };
+  if (!presentTask) {
+    return state;
   }
+
+  const updatedTask = taskReducer(presentTask, action);
+
+  if (updatedTask === presentTask) {
+    return state;
+  }
+
+  let undo = state.undo;
+
+  if (
+    UNDO_SKIP_ACTIONS[action.type] &&
+    UNDO_SKIP_ACTIONS[action.type] === state.undo.lastAction
+  ) {
+    undo = {
+      ...undo,
+      lastAction: action.type,
+    };
+  } else {
+    undo = {
+      lastAction: action.type,
+      past: [
+        ...(undo.past.length > MAX_UNDO
+          ? undo.past.slice(1, MAX_UNDO)
+          : undo.past),
+        presentTask,
+      ],
+      future: [],
+    };
+  }
+
+  return {
+    selected: updatedTask.id,
+    tasks: {
+      ...state.tasks,
+      [updatedTask.id]: updatedTask,
+    },
+    undo,
+  };
 }
