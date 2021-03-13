079287dfe41b43365dec171d51b109c152cf80c2
Thomas Hanna <thomasjhanna@gmail.com>
2021-03-13 18:01:52 +0000

    changed add event handler to use provided hook

--- app/src/components/BackButton/index.tsx
+++ app/src/components/BackButton/index.tsx
@@ -3,22 +3,17 @@ import useDispatch from 'hooks/useDispatch';
 import styles from './styles.module.css';
 import { ReactComponent as CloseIcon } from 'icons/close.svg';
 import { openTask } from 'modules/selectedScreen';
+import useEventListener from 'hooks/useEventListener';
+import keyCodes from 'utils/keyCodes';
 
 export default function BackButton() {
   const dispatch = useDispatch();
 
-  React.useEffect(() => {
-    const handleEscPress = (event: KeyboardEvent) => {
-      if(event.key === 'Escape') {
-        dispatch(openTask())
-      }
+  useEventListener('keydown', (e) => {
+    if(e.keyCode === keyCodes.esc) {
+      dispatch(openTask())
     }
-
-    window.addEventListener("keydown", handleEscPress);
-    return () => {
-      window.removeEventListener("keydown", handleEscPress);
-    }
-  }, [dispatch])
+  })
 
   return (
     <CloseIcon className={styles.button} onClick={() => dispatch(openTask())} />
