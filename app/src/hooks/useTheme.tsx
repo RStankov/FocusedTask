187986e93207bd7f7e03d9d5c9f27ca67e6892b8
TJHdev <thomasjhanna@gmail.com>
2021-03-25 15:47:39 +0000

    Update app/src/hooks/useTheme.tsx
    Co-authored-by: Radoslav Stankov <RStankov@users.noreply.github.com>

--- app/src/hooks/useTheme.tsx
+++ app/src/hooks/useTheme.tsx
@@ -4,6 +4,5 @@ import { getTheme } from 'modules/selectors';
 
 export default function useTheme() {
   const theme = useSelector(getTheme)
-  document.documentElement.setAttribute("data-theme", theme);
+  React.useEffect(() => document.documentElement.setAttribute("data-theme", theme), [theme])
 }
-
